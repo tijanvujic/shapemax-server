aa3edd164a467cd2a0526655645c943ac8b840f7
tijanvujic <tijan.vujic@gmail.com>
2025-04-17 15:47:43 +0200

    Fix database connection for Railway

--- server.js
+++ server.js
@@ -22,10 +22,11 @@ app.use(
 
 // -------- Database Setup --------
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "shapemax",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
