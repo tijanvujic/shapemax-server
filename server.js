95057510e00ce3d8e76d17a7cc116fbd75d1af87
tijanvujic <tijan.vujic@gmail.com>
2025-04-17 16:43:25 +0200

    8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 const multer = require("multer");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // -------- Middleware --------
 app.use(express.json());
